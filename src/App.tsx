e1f8235341d7a3816e6a9b23fe132c8f9ac10b9b
Jon Reese <jonreese777@gmail.com>
2022-08-18 16:48:59 -0400

    basic logic for attack component done. Worked for a bit on basic styling. Refactor and add DMG logic is next!

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,32 @@ import AttackComparison from "./components/attackComparison";
 function App() {
   return (
     <div className="App">
-      <Title>RumbleVerse Attack Priority</Title>
-      <Dialog>
-        Welcome to the Rumbleverse! This page aids RumbleVerse players in better understanding the Attack Priority
-        System. Pick two attacks then I will tell you why one move would beat another.{" "}
-      </Dialog>
+      <TitleContainer>
+        <Title>RumbleVerse Attack Priority</Title>
+        <Dialog>
+          Welcome to the Rumbleverse! This page aids RumbleVerse players in better understanding the Attack Priority
+          System. Select two attacks then I will tell you why one move would beat another.{" "}
+        </Dialog>
+      </TitleContainer>
       <AttackComparison></AttackComparison>
-
-      {moveSet.map((move) => (
-        <img src={process.env.PUBLIC_URL + move.image} key={move.image} />
-      ))}
     </div>
   );
 }
 
 export default App;
 
-const Title = styled.h1``;
-const Dialog = styled.h2``;
+const Title = styled.h1`
+  @import url("https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap");
+  font-family: "Black Han Sans", sans-serif;
+  font-size: 50px;
+  margin-bottom: 1rem;
+`;
+const Dialog = styled.h2`
+  font-size: 20px;
+`;
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 30%;
+`;
--- src/components/attackComparison.tsx
+++ src/components/attackComparison.tsx
@@ -4,14 +4,32 @@ import Paper from "@mui/material/Paper";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import styled from "styled-components";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useState, useEffect } from "react";
 import { moveSet } from "../itemInfo";
+import { answers, rankItems } from "../answers";
 
 export default function SimplePaper() {
   const [firstSelectedAttack, setFirstSelectedAttack] = useState<string>("");
   const [secondSelectedAttack, setSecondSelectedAttack] = useState<string>("");
-  const [name, setName] = React.useState("");
+  const [attackOutcome, setAttackOutcome] = useState<string>("");
+
+  useEffect(() => {
+    if (firstSelectedAttack) {
+      let condition = answerLogic();
+      console.log(condition);
+      if (condition.includes("win")) {
+        setAttackOutcome("win");
+      }
+      if (condition.includes("lose")) {
+        setAttackOutcome("lose");
+      }
+      if (condition.includes("tie")) {
+        setAttackOutcome("Tie");
+      }
+    }
+  }, [firstSelectedAttack, secondSelectedAttack]);
 
   const handleChangeOne = (event: SelectChangeEvent) => {
     setFirstSelectedAttack(event.target.value as string);
@@ -24,8 +42,45 @@ export default function SimplePaper() {
     return moveSet.map((move) => (
       <MenuItem value={move.name} key={move.name}>
         {move.name}
+        <TinyImg src={`${process.env.PUBLIC_URL + move.image}`}></TinyImg>
       </MenuItem>
     ));
+  };
+
+  console.log(attackOutcome);
+
+  const answerLogic = () => {
+    let selectedCategory = moveSet.find((move) => move.name === firstSelectedAttack)?.category.replaceAll(" ", "_");
+    let secondSelectedCategory = moveSet
+      .find((move) => move.name === secondSelectedAttack)
+      ?.category.replaceAll(" ", "_");
+    let winningAnswer = answers[`${selectedCategory}_win`];
+    let losingAnswer = answers[`${selectedCategory}_lose`];
+    let tiedAnswer = answers[`${selectedCategory}_tie`];
+    if (firstSelectedAttack === secondSelectedAttack) {
+      return tiedAnswer;
+    }
+    if (firstSelectedAttack && secondSelectedAttack === "") {
+      return winningAnswer;
+    }
+    if (firstSelectedAttack && secondSelectedAttack && selectedCategory && secondSelectedCategory) {
+      let rankOne = rankItems[selectedCategory];
+      let rankTwo = rankItems[secondSelectedCategory];
+      if (rankOne === rankTwo) {
+        return tiedAnswer;
+      }
+      if (rankOne < rankTwo) {
+        return losingAnswer;
+      }
+      if (rankOne > rankTwo) {
+        return winningAnswer;
+      }
+    }
+    if (secondSelectedAttack && firstSelectedAttack === "") {
+      return "Enter a plater attack to compare";
+    } else {
+      return "";
+    }
   };
 
   return (
@@ -36,12 +91,14 @@ export default function SimplePaper() {
         "& > :not(style)": {
           m: 1,
           width: 500,
-          height: 500,
+          height: "100%",
+          paddingBottom: "10px",
         },
       }}
     >
       <Paper elevation={3}>
         <Box sx={{ minWidth: 150, marginTop: "20px" }}>
+          <PlayersAttackText>Players Attack</PlayersAttackText>
           <FormControl sx={{ minWidth: 100 }}>
             <InputLabel id="demo-simple-select-label">Attack </InputLabel>
             <Select
@@ -54,10 +111,11 @@ export default function SimplePaper() {
               {moveSetList()}
             </Select>
           </FormControl>
-          <div>VS</div>
+          <Verses>VS</Verses>
         </Box>
         <Box sx={{ minWidth: 150, marginTop: "20px" }}>
-          <FormControl sx={{ minWidth: 100 }}>
+          <PlayersAttackText>Opponents Attack</PlayersAttackText>
+          <FormControl sx={{ minWidth: 100, marginBottom: "20px" }}>
             <InputLabel id="demo-simple-select-label">Attack</InputLabel>
             <Select
               labelId="demo-simple-select-label"
@@ -68,9 +126,45 @@ export default function SimplePaper() {
             >
               {moveSetList()}
             </Select>
+            {firstSelectedAttack ? <Outcome>Fight Outcome: {attackOutcome}</Outcome> : null}
           </FormControl>
+          {firstSelectedAttack || secondSelectedAttack ? (
+            <Paper
+              elevation={3}
+              sx={{ width: "75%", display: "flex", justifyContent: "center", padding: 2, margin: "0 auto" }}
+            >
+              <Answer>{answerLogic()}</Answer>
+            </Paper>
+          ) : null}
         </Box>
       </Paper>
     </Box>
   );
 }
+
+const TinyImg = styled.img`
+  width: 40px;
+  margin-left: 10px;
+`;
+const Answer = styled.div`
+  width: 40px;
+  margin-left: 10px;
+  width: 90%;
+`;
+const Verses = styled.div`
+  margin-top: 1rem;
+  font-size: x-large;
+  font-weight: 600;
+`;
+const PlayersAttackText = styled.div`
+  margin-top: 1rem;
+  font-size: x-large;
+  font-weight: 600;
+  margin-bottom: 1em;
+`;
+const Outcome = styled.div`
+  margin-top: 1rem;
+  font-size: x-large;
+  font-weight: 600;
+  margin-bottom: 1em;
+`;
